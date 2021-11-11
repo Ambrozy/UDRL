2acf6e05c0b73551ea2e98f6b43ea9bd4218aa19
Ambrozy <bushmanov.vova@yandex.ru>
2021-11-11 20:49:04 +0500

    fix imports

--- framework/Driver.py
+++ framework/Driver.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from framework import BehaviorFunction, ReplayBuffer
+from framework.BehaviorFunction import BehaviorFunction
+from framework.ReplayBuffer import ReplayBuffer
 
 
 class Driver:
